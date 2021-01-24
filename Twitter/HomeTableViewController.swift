e4b90107c7a83021536ddaf357f71328dc21f0cc
zach dorame barajas <zachdoramebarajas@gmail.com>
2021-01-24 12:04:34 -0800

    Implemented infinite scroll

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -39,9 +39,10 @@ class HomeTableViewController: UITableViewController {
         return cell
     }
     
-    @objc func loadTweet(){
+    @objc func loadTweets(){
+        numberofTweet = 20
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
-        let myParams = ["count": 10]
+        let myParams = ["count": numberofTweet]
         
         TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
             
@@ -57,10 +58,34 @@ class HomeTableViewController: UITableViewController {
         })
     }
     
+    func loadMoreTweets(){
+        let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
+        numberofTweet = numberofTweet + 20
+        let myParams = ["count": numberofTweet]
+        
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+            
+            self.tweetArray.removeAll()
+            for tweet in tweets {
+                self.tweetArray.append(tweet)
+            }
+            self.tableView.reloadData()
+            
+        }, failure: { (Error) in
+            print("Could not retreive tweets")
+        })
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if indexPath.row + 1 == tweetArray.count{
+            loadMoreTweets()
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweet()
-        myRefreshControl.addTarget(self, action: #selector(loadTweet), for: .valueChanged)
+        loadTweets()
+        myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
     }
 
